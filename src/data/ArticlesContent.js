aeadd3e02e39f501652b75d829008b7ff5e43882
pockks <petervolosenco@gmail.com>
2022-03-15 13:52:22 +0000

    1 file modified, 1 added

---
+++ src/data/ArticlesContent.js
@@ -0,0 +1,26 @@
+export default [
+  {
+    Id: 'dbfb8245-6f2f-4d31-96f4-f7764ebb3e0b',
+    Name: 'Test 1',
+    Text: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras porta sem finibus eros sodales, a auctor sem rutrum. Fusce pharetra blandit mauris, ac ornare nunc semper vel.',
+    Order: 1,
+  },
+  {
+    Id: '898a4bbc-74d9-4a2d-8b54-6b1660379320',
+    Name: 'Test 2',
+    Text: 'Nam tincidunt sollicitudin nulla, sit amet suscipit ante malesuada in. Nulla sit amet nisi vel massa lacinia dictum sit amet id velit. Integer vel massa eros. ',
+    Order: 2,
+  },
+  {
+    Id: '898a4bbc-74d9-4a2d-8b54-6b1660379321',
+    Name: 'Test 3',
+    Text: 'Nam tincidunt sollicitudin nulla, sit amet suscipit ante malesuada in. Nulla sit amet nisi vel massa lacinia dictum sit amet id velit. Integer vel massa eros. ',
+    Order: 3,
+  },
+  {
+    Id: '898a4bbc-74d9-4a2d-8b54-6b1660379322',
+    Name: 'Test 4',
+    Text: 'Nam tincidunt sollicitudin nulla, sit amet suscipit ante malesuada in. Nulla sit amet nisi vel massa lacinia dictum sit amet id velit. Integer vel massa eros. ',
+    Order: 4,
+  },
+];
